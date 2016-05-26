dd54f6f006ce3510f3888cb86a4c33fafb25e985
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-05-26 10:37:50 +0200

    add environ setting

--- widukind_web/settings.py
+++ widukind_web/settings.py
@@ -7,7 +7,7 @@ class Config(object):
     
     BOOTSTRAP_SERVE_LOCAL = True
     
-    DEFAULT_URL_API = "http://widukind-api.cepremap.org/api/v1"
+    DEFAULT_URL_API = config('WIDUKIND_BASE_URL_API', "http://widukind-api.cepremap.org/api/v1")
     BASE_URL_API_JSON = config('WIDUKIND_BASE_URL_API_JSON', "%s/json" % DEFAULT_URL_API)
     BASE_URL_API_SDMX = config('WIDUKIND_BASE_URL_API_SDMX', "%s/sdmx" % DEFAULT_URL_API)
     BASE_URL_API_HTML = config('WIDUKIND_BASE_URL_API_HTML', "%s/html" % DEFAULT_URL_API)
